b1ab80388e8b25e45187d2f75ef29348f58e2180
Solly <solobarine@gmail.com>
2022-11-24 20:16:09 +0100

    fix linters

--- src/__tests__/Home.test.js
+++ src/__tests__/Home.test.js
@@ -1,4 +1,4 @@
-import { screen, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Home from '../pages/Home';
 import '@testing-library/jest-dom';
