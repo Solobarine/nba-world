f619378e284e2b32a59f78bf401a833c40297b80
Solly <solobarine@gmail.com>
2022-11-25 20:45:30 +0100

    Fix linter errors

--- src/components/AtlanticTeams.js
+++ src/components/AtlanticTeams.js
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom';
 
-const AtlanticTeams = () => { // eslint-disable-line
-  return (
+const AtlanticTeams = () => (
     <div className="atlantic">
       <Link to="/home/atlantic"><i className="fa-solid fa-arrow-right-long" /></Link>
       <h2 className="c-title">Atlantic Division Teams</h2>
     </div>
-  );
-};
+);
 
 export default AtlanticTeams;
--- src/components/NorthWestTeams.js
+++ src/components/NorthWestTeams.js
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom';
 
-const NorthWestTeams = () => { // eslint-disable-lie
-  return (
+const NorthWestTeams = () => (
     <div className="northwest">
       <Link to="/home/northwest"><i className="fa-solid fa-arrow-right-long" /></Link>
       <h2 className="c-title">NorthWest Division Teams</h2>
     </div>
-  );
-};
+);
 
 export default NorthWestTeams;
