7023c47390a8ac26b7fcebd16e8ada67dda0a3a7
Akpuru Solomon Barine <solobarine@gmail.com>
2022-11-26 15:40:30 +0100

    Update Nba.js

--- src/components/Nba.js
+++ src/components/Nba.js
@@ -1,10 +1,8 @@
-const Nba = () => { // eslint-disable-line
-  return (
-    <div className="nba-section">
-      <img src="#" alt="" className="logo" />
-      <h2 className="title">NBA</h2>
-    </div>
-  );
-};
+const Nba = () => (
+  <div className="nba-section">
+    <img src="#" alt="" className="logo" />
+    <h2 className="title">NBA</h2>
+  </div>
+);
 
 export default Nba;
