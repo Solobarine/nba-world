714508e3f315443f188850f642e84816150f3ef1
Akpuru Solomon Barine <solobarine@gmail.com>
2022-11-26 15:42:45 +0100

    Update NbaTeams.js

--- src/components/NbaTeams.js
+++ src/components/NbaTeams.js
@@ -1,12 +1,10 @@
 import { Link } from 'react-router-dom';
 
-const NbaTeams = () => { //eslint-disable-line
-  return (
-    <div className="all">
-      <Link to="/home/all"><i className="fa-solid fa-arrow-right-long" /></Link>
-      <h2 className="all-teams">All NBA Teams</h2>
-    </div>
-  );
-};
+const NbaTeams = () => (
+  <div className="all">
+    <Link to="/home/all"><i className="fa-solid fa-arrow-right-long" /></Link>
+    <h2 className="all-teams">All NBA Teams</h2>
+  </div>
+);
 
 export default NbaTeams;
